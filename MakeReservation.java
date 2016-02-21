3addc7c2fc1f269744426076a08b9a0cee8c1265
ql657195490 <qilei753@yahoo.com>
2016-02-20 23:00:00 -0500

    Update MakeReservation.java

--- MakeReservation.java
+++ MakeReservation.java
@@ -1,22 +1,20 @@
 import java.awt.EventQueue;
+import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import java.awt.BorderLayout;
+
 import java.awt.Color;
+
 import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.JComboBox;
-import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 
-public class MakeReservation {
+public class MakeReservation_5 {
 
     private JFrame frame;
-    private JTextField textField;
 
     /**
      * Launch the application.
@@ -25,7 +23,7 @@ public class MakeReservation {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
-                    MakeReservation window = new MakeReservation();
+                    MakeReservation_5 window = new MakeReservation_5();
                     window.frame.setVisible(true);
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -37,7 +35,7 @@ public class MakeReservation {
     /**
      * Create the application.
      */
-    public MakeReservation() {
+    public MakeReservation_5() {
         initialize();
     }
 
@@ -56,73 +54,35 @@ public class MakeReservation {
         frame.getContentPane().add(panel);
         panel.setLayout(null);
         
-        JPanel panel_1 = new JPanel();
-        panel_1.setBounds(0, 0, 450, 400);
-        frame.getContentPane().add(panel_1);
+        JLabel lblNewLabel = new JLabel("Confirmation");
+        lblNewLabel.setForeground(Color.ORANGE);
+        lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
+        lblNewLabel.setBounds(175, 20, 100, 20);
+        panel.add(lblNewLabel);
         
-        JPanel panel_2 = new JPanel();
-        panel_2.setBounds(0, 0, 10, 10);
-        frame.getContentPane().add(panel_2);
+        JLabel lblNewLabel_1 = new JLabel("Reservation ID");
+        lblNewLabel_1.setBounds(40, 60, 100, 20);
+        panel.add(lblNewLabel_1);
         
-        JPanel panel_3 = new JPanel();
-        panel_3.setBounds(0, 0, 10, 10);
-        frame.getContentPane().add(panel_3);
+        JLabel label = new JLabel("12345");
+        label.setBounds(234, 62, 61, 16);
+        panel.add(label);
         
-        JPanel panel_4 = new JPanel();
-        panel_4.setBounds(0, 0, 10, 10);
-        frame.getContentPane().add(panel_4);
+        JLabel lblNewLabel_2 = new JLabel("Thank you for your purchase! Please save reservation ID for your records");
+        lblNewLabel_2.setBounds(40, 90, 350, 20);
+        lblNewLabel_2.setFont(new Font("Lucida Grande", Font.PLAIN, 9));
+        panel.add(lblNewLabel_2);
         
-        panel.setVisible(true);
-        panel_1.setVisible(false);
-        panel_2.setVisible(false);
-        panel_3.setVisible(false);
-        panel_4.setVisible(false);
-        
-        JLabel lblSearchTrain = new JLabel("Search Train");
-        lblSearchTrain.setForeground(Color.ORANGE);
-        lblSearchTrain.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
-        lblSearchTrain.setBounds(180, 20, 90, 20);
-        panel.add(lblSearchTrain);
-        
-        JLabel lblDeportsFrom = new JLabel("Deports From");
-        lblDeportsFrom.setBounds(70, 80, 90, 20);
-        panel.add(lblDeportsFrom);
-        
-        JLabel lblArrivesAt = new JLabel("Arrives At");
-        lblArrivesAt.setBounds(70, 120, 70, 20);
-        panel.add(lblArrivesAt);
-        
-        JLabel lblDepatureDate = new JLabel("Depature Date");
-        lblDepatureDate.setBounds(70, 160, 90, 20);
-        panel.add(lblDepatureDate);
-        
-        JComboBox comboBox = new JComboBox();
-        comboBox.setBounds(242, 78, 110, 27);
-        panel.add(comboBox);
-        
-        JComboBox comboBox_1 = new JComboBox();
-        comboBox_1.setBounds(242, 118, 161, 27);
-        panel.add(comboBox_1);
-        
-        textField = new JTextField();
-        textField.setBounds(242, 160, 110, 20);
-        panel.add(textField);
-        textField.setColumns(10);
-        
-        JButton btnNewButton = new JButton("New button");
-        btnNewButton.setBounds(364, 157, 44, 29);
-        panel.add(btnNewButton);
-        
-        JButton btnNewButton_1 = new JButton("Find Train");
-        btnNewButton_1.addActionListener(new ActionListener() {
+        JButton btnNewButton = new JButton("Go back to choose funtionality");
+        btnNewButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                panel.setVisible(false);
-                panel_1.setVisible(true);
+                frame.dispose();
+                CustomerFunctionalities cf = new CustomerFunctionalities();
+                cf.cfWindow();
             }
         });
-        btnNewButton_1.setBounds(286, 310, 100, 29);
-        panel.add(btnNewButton_1);
-        
-        
+        btnNewButton.setBounds(115, 218, 220, 29);
+        panel.add(btnNewButton);
     }
+
 }
